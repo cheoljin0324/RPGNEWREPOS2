43c57d6781f1f043f8902aa0aeaece7d84c72ff7
DESKTOP-OHD6DA9\USER <cjfwls05324@naver.com>
2022-06-27 10:58:46 +0900

    플레이어 UGUI 추가중

---
+++ unityRPGPRACTICE/Assets/Script/PlayerUIManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image FirstHeart;
+    [SerializeField]
+    private Image SecondHeart;
+    [SerializeField]
+    private Image ThirdHeart;
+}
